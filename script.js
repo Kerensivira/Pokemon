228ec6097e68fc51eb9ce58dd5675ad9fe555094
Alexandra Sivira <kerensivira@gmail.com>
2022-03-03 19:15:52 -0500

    564

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 
-// Variable de la constante.
+// Variable de la API's.
 
-const urlAPIsPokemon = 'https://pokeapi.co/'
+ const urlAPIpokemon = 'https://pokeapi.co/api/v2/pokemon/';
 
-//Variables HTML.
+ //Variables HTML.
 
-const Form = document.getElementById('form')
-const Search = document.getElementById('search')
-const Submit = document.getElementById('submit')
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+ const submit = document.getElementById('submit');
+ const main = document.getElementById('main');
 
+const getPokemons = async (url,id) => {
+    const res = await fetch (url + id)
+    const data = await res.json()
+    console.log(data);
+    //showPokemons(data.results);
+
+}
+
+ function TraerPokemones (number) {
+
+    for (let i = 1; i <= number; i++) {
+       
+        getPokemons(urlAPIpokemon, i)
+        
+    }
+ }
+
+ TraerPokemones(20);
+
+
+function CrearPokemons(pokemon) {
+    
+
+
+}
+ 
+
+// const showPokemons = (pokemons) => {
+
+//     main.interHTML =''
+//     movies.forEach( pokemons => {
+        
+//     });
+// }
 
